5482a7378eb8dce166f17643223f010dfbcc4e94
Denis <razoranet@gmail.com>
2022-01-06 17:40:50 +0300

    feedback + heroku

--- src/Feedback/Feedback.tsx
+++ src/Feedback/Feedback.tsx
@@ -1,10 +1,37 @@
 import styles from "./Feedback.module.scss";
 import container from "../common/styles/Container.module.css";
-import React from "react";
+import React, {ChangeEvent, useState} from "react";
 import {Title} from "../common/components/title/Title";
 import {Fade} from "react-awesome-reveal";
+import axios from "axios";
 
 export const Feedback = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [text, setText] = useState('')
+
+
+    const handlerName = (e: ChangeEvent<HTMLInputElement>) => {
+        setName(e.currentTarget.value)
+    }
+    const handlerEmail = (e: ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.currentTarget.value)
+    }
+    const handlerText = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setText(e.currentTarget.value)
+    }
+    const handlerClick = () => {
+        axios.post('https://smtpnodeserver.herokuapp.com/sendMessage',{
+            name, email, text
+        })
+        setTimeout(()=> {
+            setName('')
+            setEmail('')
+            setText('')
+        },500)
+        alert('Ваша сообщение отправлено!')
+    }
+
     return (
         <div className={styles.main} id={'contacts'}>
             <Fade>
@@ -12,12 +39,14 @@ export const Feedback = () => {
                     <Title title={'Contacts'}/>
                     <form className={styles.form}>
                         <div className="text-field__icon text-field__icon_email">
-                            <input placeholder={"Your Name"} className={styles.inputs}/>
+                            <input placeholder={"Your Name"} className={styles.inputs} onChange={handlerName}
+                                   value={name}/>
                         </div>
-                        <input placeholder={"Your Email"} className={styles.inputs}/>
-                        <textarea placeholder={"Your Message"} className={styles.textarea}/>
+                        <input placeholder={"Your Email"} className={styles.inputs} onChange={handlerEmail} value={email}/>
+                        <textarea placeholder={"Your Message"} className={styles.textarea}
+                                  onChange={handlerText} value={text}/>
                     </form>
-                    <button type='submit'>Send</button>
+                    <button type='submit' onClick={handlerClick}>Send</button>
                 </div>
             </Fade>
         </div>
